29896b5a3b90d563f5d61825a9b701c1453a6334
Nightstalker <Nightstalker@Benjamin.fritz.box>
2015-07-06 20:54:57 +0200

    added override methods onRestart onDestroy

--- src/P/I/M/Basilia.java
+++ src/P/I/M/Basilia.java
@@ -12,8 +12,27 @@ public class Basilia extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    // Called when user is 
+    // switching back to app 
+    // after first creation
+    // i.e. just hides the app
+    @Override
+    public void onRestart()
+    {
+        super.onRestart();
+        // implement smth
+    }
     
-    
+    // Called when user forcing
+    // app to be exited
+    // or maybe smth weird happened ^_°
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        //implement smth
+        
+    }
     
     
 }
