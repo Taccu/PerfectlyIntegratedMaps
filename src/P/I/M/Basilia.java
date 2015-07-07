7b2d1bc570a9e3f114009a0b24f869e5dcd528e9
Nightstalker <Nightstalker@Benjamin.fritz.box>
2015-07-07 11:53:20 +0200

    added override methods onRestart onDestroy

--- src/P/I/M/Basilia.java
+++ src/P/I/M/Basilia.java
@@ -1,16 +1,22 @@
 package P.I.M;
 
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import java.util.ArrayList;
 
 public class Basilia extends Activity
 {
+    // array which contains all locations
+    private ArrayList<BLocation> blocations;
+    private Thread t;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
     }
     // Called when user is 
     // switching back to app 
@@ -21,6 +27,7 @@ public class Basilia extends Activity
     {
         super.onRestart();
         // implement smth
+        
     }
     
     // Called when user forcing
@@ -33,6 +40,27 @@ public class Basilia extends Activity
         //implement smth
         
     }
-    
-    
+    private ArrayList<BLocation> getLocationsFromDB(final ArrayList<BLocation> blocations){
+        
+        // getting locations runs in a thread
+        t = new Thread(new Runnable()
+        {
+            @Override
+            public void run(){
+                // do smth
+                final Location location = new Location("yourprovidername");
+                location.setLatitude(1.2345d);
+                location.setLongitude(1.2345d);
+                blocations.add(new BLocation("asd",location, "Sample description", 0));
+            }
+        });
+        // end of thread
+        return blocations;
+    }
+    // This method should be called 
+    // everytime app starts, restarts etc.
+    private boolean checkStatusOfApp()
+    {
+        return true;
+    }
 }
